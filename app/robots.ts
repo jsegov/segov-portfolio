23c8183c425b3ea471c526b06cc57a334f74b1ba
Jonathan Segovia <jonathan.j.segovia@gmail.com>
2025-02-01 19:16:42 -0800

    fixed robot.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,10 @@
-export const robots = {
-  rules: [
-    {
-      userAgent: '*',
-    },
-  ],
-  sitemap: `${process.env.NEXT_PUBLIC_URL || 'https://segov.app'}/sitemap.xml`,
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_URL || 'https://segov.app'}/sitemap.xml`,
+  }
 }

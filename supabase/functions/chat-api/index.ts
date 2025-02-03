d6e69c1b6fcdc4e7dbb9a4fe14e907a39ec2aa28
Jonathan Segovia <jonathan.j.segovia@gmail.com>
2025-02-02 19:27:29 -0800

    updated name of edge function tests

--- supabase/functions/chat-api/index.ts
+++ supabase/functions/chat-api/index.ts
@@ -174,15 +174,3 @@ export async function handler(req: Request): Promise<Response> {
 }
 
 serve(handler);
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/chat-api' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
